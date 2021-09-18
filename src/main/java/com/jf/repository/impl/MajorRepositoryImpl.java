22215a4773d3e5466390c503a00bb9a112f0432b
thannhattruong99 <truongtnse130426@fpt.edu.vn>
2021-09-18 16:14:36 +0700

    Remove library

--- src/main/java/com/jf/repository/impl/MajorRepositoryImpl.java
+++ src/main/java/com/jf/repository/impl/MajorRepositoryImpl.java
@@ -11,7 +11,6 @@ import java.util.List;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
